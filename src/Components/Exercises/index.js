93ead8b99c98dce8d7ba6f2e690cf78c2bb2d6fa
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-03-20 12:41:17 +0100

    basic skeleton structure done

---
+++ src/Components/Exercises/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Exercises extends Component {
+  render() {
+    return (
+      <div>
+        Hello from Exercises
+      </div>
+    );
+  }
+}
+
+export default Exercises;
